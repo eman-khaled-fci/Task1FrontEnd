04cdb3cd6ab325b1eb75b703158e95e6f75169db
eman-khaled-fci <mony.5aled.fci@gmail.com>
2023-12-16 23:28:30 +0200

    init

---
+++ script.js
@@ -0,0 +1,103 @@
+let csvData;
+
+function readCSV() {
+    const fileInput = document.getElementById('fileInput');
+    const file = fileInput.files[0];
+
+    if (file) {
+        const formData = new FormData();
+        formData.append('file', file);
+
+       
+        fetch('http://localhost:4000/api/csv/upload', {
+            method: 'POST',
+            body: formData
+        })
+            .then(response => response.text())
+            .then(data => {
+                console.log(data);
+                
+                getCsvData();
+            })
+            .catch(error => {
+                console.error('Error uploading CSV file:', error);
+            });
+    }
+}
+
+function getCsvData() {
+  
+    fetch('http://localhost:4000/api/csv/get-csv-data')
+        .then(response => response.json())
+        .then(data => {
+           
+            csvData = data;
+          
+            displayGrid(csvData);
+        })
+        .catch(error => {
+            console.error('Error getting CSV data:', error);
+        });
+}
+
+function displayGrid(data) {
+    const gridContainer = document.getElementById('gridContainer');
+
+    // Clear existing content in the grid container
+    gridContainer.innerHTML = '';
+
+    // Create a table element
+    const table = document.createElement('table');
+    table.border = '1';
+
+    // Create the header row
+    const headerRow = document.createElement('tr');
+    Object.keys(data[0]).forEach(key => {
+        const th = document.createElement('th');
+        th.textContent = key;
+        headerRow.appendChild(th);
+    });
+    table.appendChild(headerRow);
+
+    // Create rows with data
+    data.forEach(rowData => {
+        const row = document.createElement('tr');
+        Object.values(rowData).forEach(value => {
+            const td = document.createElement('td');
+            td.textContent = value;
+            row.appendChild(td);
+        });
+        table.appendChild(row);
+    });
+
+    // Append the table to the grid container
+    gridContainer.appendChild(table);
+}
+
+function downloadXLSX() {
+    fetch('http://localhost:4000/api/csv/convert-to-excel')
+        .then(response => response.blob())
+        .then(blob => {
+
+            const url = window.URL.createObjectURL(new Blob([blob]));
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = 'converted.xlsx';
+
+
+            document.body.appendChild(a);
+
+
+            a.click();
+
+
+            document.body.removeChild(a);
+
+
+            console.log('Downloading XLSX...');
+        })
+        .catch(error => {
+            console.error('Error converting to Excel:', error);
+        });
+}
+
